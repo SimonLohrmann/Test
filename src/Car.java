736245473b312a13d71a5a699b5d39f2735752a4
violetamorina-zeqiraj <morina-zeqiraj@live.com>
2023-11-24 21:38:31 +0100

    Add Car class

--- src/Car.java
+++ src/Car.java
@@ -1,18 +1,22 @@
+
 public class Car extends Vehicle{
 
 
-    @Override
-    public void start() {
 
-    }
+        @Override
+        public void start () {
+            System.out.println("brum brum");
+        }
 
-    @Override
-    public void doBreak() {
+        @Override
+        public void doBreak () {
+            System.out.println("stop");
+        }
 
-    }
+        @Override
+        public void accelerate () {
+            System.out.println("start");
+        }
 
-    @Override
-    public void accelerate() {
 
-    }
 }
